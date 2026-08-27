dde8e26b786df06fb02eb52b60bd4af4d062e4f7
rubengitdev <rubencahyadi504@gmail.com>
2026-08-27 14:39:07 +0700

    Refactor: Restructure files for feature-based development

--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -1,20 +1,23 @@
-import React from "react";
+import React from 'react';
 
 function DisplaySection({ array, InfoComponent, title }) {
-  return (
-    <React.Fragment>
-      {
-        // Show resume header if at least one item in array is not hidden.
-        !array.every((obj) => obj.isHidden) && (
-          <h3 className="header-text">{title}</h3>
-        )
-      }
+    return (
+        <>
+            {
+                // Show resume header if at least one item in array is not hidden.
+                !array.every((obj) => obj.isHidden) && (
+                    <h3 className="header-text">{title}</h3>
+                )
+            }
 
-      {array.map(
-        (info) => !info.isHidden && <InfoComponent info={info} key={info.id} />
-      )}
-    </React.Fragment>
-  );
+            {array.map(
+                (info) =>
+                    !info.isHidden && (
+                        <InfoComponent info={info} key={info.id} />
+                    ),
+            )}
+        </>
+    );
 }
 
 export default DisplaySection;

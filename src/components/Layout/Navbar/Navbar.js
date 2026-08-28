b991bfb8ff4d3b46992c425009ed577c381dd2ba
rubengitdev <rubencahyadi504@gmail.com>
2026-08-28 14:05:01 +0700

    Refactor: Adjust styling

--- src/components/Layout/Navbar/Navbar.js
+++ src/components/Layout/Navbar/Navbar.js
@@ -3,19 +3,28 @@ import './Navbar.css';
 export default function Navbar({ onGoToPage, page }) {
     return (
         <nav className="navbar">
+            {/* Input Resume Button */}
             <button
                 onClick={() => onGoToPage('content')}
                 data-selected={page === 'content'}
             >
                 <i className="fa-regular fa-file-lines" />
-                Content
+                Input Resume
             </button>
+
+            {/* Customize Button */}
             <button
                 onClick={() => onGoToPage('customize')}
                 data-selected={page === 'customize'}
             >
                 <i className="fa-solid fa-pen-ruler" />
                 Customize
+            </button>
+
+            {/* Download Button */}
+            <button>
+                <i className="fa-solid fa-download" />
+                Download
             </button>
         </nav>
     );
